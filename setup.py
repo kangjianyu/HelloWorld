c4c26916046a1c4d1cd018d8a31a41c2a54d4944
kangjianyu <609363537@qq.com>
2019-02-03 21:21:59 +0800

    change

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
         description='This is a test of the setup',   # 简单描述
         author='kjy',  # 作者
         author_email='609363537@qq.com',  # 作者邮箱
-        url='www.baidu.com',      # 包的主页
+        url='https://github.com/kangjianyu/HelloWorld.git',      # 包的主页
         packages=find_packages(),                 # 包
 )
